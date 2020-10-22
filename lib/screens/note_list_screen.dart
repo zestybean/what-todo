4c94a4fbbf2d79990a06c64ee331278874a95a8b
Brando Lugo <brando.lugo@gmail.com>
2020-10-22 11:00:21 -0500

    Initial app ui

--- lib/screens/note_list_screen.dart
+++ lib/screens/note_list_screen.dart
@@ -3,10 +3,33 @@
 //Flutter
 import 'package:flutter/material.dart';
 
+//Helpers
+import '../helper/helpers.dart';
+
+//Packages
+import 'package:provider/provider.dart';
+
 class NoteListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //FutureBuilder depends on state which will determine UI
-    return FutureBuilder();
+    return FutureBuilder(
+      future: Provider.of<NoteProvider>(context, listen: false).getNotes(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        } else {
+          return Scaffold(
+            body: Container(
+              child: Text('We here!'),
+            ),
+          );
+        }
+      },
+    );
   }
 }
