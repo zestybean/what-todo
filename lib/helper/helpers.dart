f6dfbe81e2276d0de91ec52d1607139a1b69583c
Brando Lugo <brando.lugo@gmail.com>
2020-10-22 09:48:44 -0500

    Added the note provider to retrieve data from the db and return a converted list of notes

---
+++ lib/helper/helpers.dart
@@ -0,0 +1,3 @@
+//Import this instead of creating individual imports
+export 'database_helper.dart';
+export 'note_provider.dart';
--- lib/helper/note_provider.dart
+++ lib/helper/note_provider.dart
@@ -1,1 +1,33 @@
 //Contains add update delete oprations to notify provider listeners
+
+//Flutter
+import 'package:flutter/material.dart';
+
+//Helper
+import 'database_helper.dart';
+
+//Models
+import '../models/note.dart';
+
+class NoteProvider with ChangeNotifier {
+  List _items = [];
+
+  List get items {
+    return [..._items];
+  }
+
+  //Retrieve notes from the DB after converting map to list
+  Future getNotes() async {
+    final notesList = await DatabaseHelper.getNotesFromDB();
+
+    _items = notesList
+        .map(
+          (n) => Note(n['id'], n['title'], n['content'], n['imagePath']),
+        )
+        .toList();
+
+    //Will trigger rebuild to listeners
+    //Used mainly by the provider package
+    notifyListeners();
+  }
+}
