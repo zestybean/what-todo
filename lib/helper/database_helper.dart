172635ec0eec16a309be0fb8d1e1eb47483d02e1
Brando Lugo <brando.lugo@gmail.com>
2020-10-22 09:35:01 -0500

    Added the DB helper to connect the sql DB to flutter

--- lib/helper/database_helper.dart
+++ lib/helper/database_helper.dart
@@ -1,1 +1,32 @@
 //Database related operations
+
+//Packages
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static Future database() async {
+    ///Stores db location of both android and
+    ///ios dir location to be join with the db string
+    final databasePath = await getDatabasesPath();
+
+    return openDatabase(
+      //Joins both db path string and notes db string together
+      join(databasePath, 'notes_database.db'),
+      onCreate: (database, version) {
+        //SQL command storing id as int and primary key and rest of notes properties
+        return database.execute(
+            'CREATE TABLE notes(id INTEGER PRIMARY KEY, title TEXT, context, TEXT, imagePath TEXT)');
+      },
+      version: 1,
+    );
+  }
+
+  //Create db instance and query db in descending order
+  static Future<List<Map<String, dynamic>>> getNotesFromDB() async {
+    final database = await DatabaseHelper.database();
+    //ID is in datetime so it cna be sorted in descending order of dates
+    //final return is a list of map values
+    return database.query('notes', orderBy: 'id DESC');
+  }
+}
